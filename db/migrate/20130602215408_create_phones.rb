97f35a1385dce96fe32f6decbee16da638993580
krystalmm <taeyeon_aina@live.jp>
2021-01-04 22:58:57 +0900

    [update] appディレクトリ

--- db/migrate/20130602215408_create_phones.rb
+++ db/migrate/20130602215408_create_phones.rb
@@ -1,4 +1,4 @@
-class CreatePhones < ActiveRecord::Migration
+class CreatePhones < ActiveRecord::Migration[6.0]
   def change
     create_table :phones do |t|
       t.references :contact, index: true
