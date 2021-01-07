ac57cc1c37aa9d2056939403fb73562b101a2d2d
krystalmm <taeyeon_aina@live.jp>
2021-01-07 13:07:51 +0900

    [update] projects_controller_spec.rb (add edit)

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -278,4 +278,59 @@ RSpec.describe ProjectsController, type: :controller do
             end
         end
     end
+
+    describe "#edit" do
+        # 認可されているユーザーとして
+        context "as an authorized user" do
+
+            before do
+                @user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: @user)
+            end
+
+            # 正常にレスポンスを返すこと
+            it "responds successfully" do
+                sign_in @user
+                get :edit, params: { id: @project.id }
+                expect(response).to be_successful
+            end 
+        end
+
+        # 認可されていないユーザーとして
+        context "as an unauthorized user" do
+
+            before do
+                @user = FactoryBot.create(:user)
+                other_user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: other_user)
+            end
+
+            # ダッシュボードにリダイレクトすること
+            it "redirects to the dashborad" do
+                sign_in @user
+                get :edit, params: { id: @project.id }
+                expect(response).to redirect_to root_path
+            end
+        end
+
+        # ゲストとして
+        context "as a guest" do
+
+            before do
+                @project = FactoryBot.create(:project)
+            end
+
+            # 302レスポンスを返すこと
+            it "returns a 302 response" do
+                get :edit, params: { id: @project.id }
+                expect(response).to have_http_status "302"
+            end
+
+            # サインイン画面にリダイレクトすること
+            it "redirects to the sign-in page" do
+                get :edit, params: { id: @project.id }
+                expect(response).to redirect_to "/users/sign_in"
+            end
+        end
+    end
 end
