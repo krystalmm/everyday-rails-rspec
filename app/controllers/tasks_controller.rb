b77382a83c586f93b21f9af8eec90fe394cd2c79
krystalmm <taeyeon_aina@live.jp>
2021-01-07 12:29:32 +0900

    [add] tasks_controller_spec.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,4 +38,45 @@ class TasksController < ApplicationController
         end
       end
     end
-  
+
+
+    # PATCH/PUT /tasks/1
+    # PATCH/PUT /tasks/1.json
+    def update
+      respond_to do |format|
+        if @task.update(task_params)
+         format.html { redirect_to @project, notice: 'Task was successfully updated.' }
+         format.json { render :show, status: :ok, location: @task }
+        else
+          format.html { render :edit }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # DELETE /tasks/1
+    # DELETE /tasks/1.json
+    def destroy
+      @task.destroy
+      respond_to do |format|
+        format.html { redirect_to @project, notice: 'Task was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+
+    def toggle
+      @task.update_attributes(completed: !@task.completed)
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_task
+        @task = Task.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def task_params
+        params.require(:task).permit(:name, :project_id, :completed)
+      end
+      
+end 
