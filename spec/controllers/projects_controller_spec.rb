3b0e4fd48f2b0e761f1fc9e72a3c8e85804118fc
krystalmm <taeyeon_aina@live.jp>
2021-01-07 12:53:21 +0900

    [update] projects_controller_spec.rb (add new)

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -245,4 +245,37 @@ RSpec.describe ProjectsController, type: :controller do
             end
         end
     end
+
+    describe "#new" do
+        # 認証済みのユーザーとして
+        context "as an authenticated user" do
+
+            before do
+                @user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: @user)
+            end
+
+            # 正常にレスポンスを返すこと
+            it "responds successfully" do
+                sign_in @user
+                get :new
+                expect(response).to be_successful
+            end
+        end
+
+        # ゲストとして
+        context "as a guest" do
+            # 302レスポンスを返すこと
+            it "returns a 302 response" do
+                get :new
+                expect(response).to have_http_status "302"
+            end
+
+            # サインイン画面にリダイレクトすること
+            it "redirects to the sign-in page" do
+                get :new
+                expect(response).to redirect_to "/users/sign_in"
+            end
+        end
+    end
 end
